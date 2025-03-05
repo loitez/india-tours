ae97e82894f55053d119346ea71d9fd63d14d360
loitez <loitez@mail.ru>
2025-03-05 15:36:48 +0300

    fix wrapper styles and html height

--- src/components/Wrappers/Wrapper.tsx
+++ src/components/Wrappers/Wrapper.tsx
@@ -1,9 +1,10 @@
+import styles from './Wrappers.module.scss'
 import {wrappersMap} from "../../constants/wrappers.ts";
 import {WrapperProps} from "../../types/wrappers.types.ts";
 
 
 export const Wrapper = ({children, wrapperType = "block", className}: WrapperProps) => {
-    const classNames = [className, wrappersMap[wrapperType]].filter(Boolean).join(" ");
+    const classNames = [className, wrappersMap[wrapperType], styles].filter(Boolean).join(" ");
 
     return (
         <div className={classNames}>
